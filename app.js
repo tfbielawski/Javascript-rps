27f77a7dded65f90dbd1bea516e6e30575883e27
Tom Bielawski <tfbielawski@protonmail.com>
2024-01-26 11:09:46 -0500

    Added game logic for win, lose, tie scenarios

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const PLAYER_WINS = "PLAYER_WINS";
+const COMPUTER_WINS = "COMPUTER WINS";
 
 let gameIsRunning = false;
 
 const getPlayerChoice = function(){
     
     //JS prompt method, two args
-    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, "").toUpperCase();
+    const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, " ").toUpperCase();
+    alert(`You chose ${selection}`)
     if (
         selection !== ROCK && 
         selection !== PAPER && 
@@ -19,14 +23,39 @@ const getPlayerChoice = function(){
             alert(`Invalid choice. We chose ${ROCK} for you.`);
             return DEFAULT_USER_CHOICE;
         }
+    return selection;
 };
+
+const getComputerChoice = function(){
+    const randomValue = Math.random();
+    if (randomValue <= 0.34) {return ROCK;}
+    else if (randomValue <= 0.67) {return PAPER;}
+    else {return SCISSORS;}
+}
+
+const getWinner = function(cChoice, pChoice){
+    if (cChoice === pChoice){ return RESULT_DRAW; }
+    else if (
+        cChoice === ROCK && pChoice === PAPER || 
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+    ){return PLAYER_WINS}
+    else if (
+        pChoice === ROCK && cChoice === PAPER || 
+        pChoice === PAPER && cChoice === SCISSORS ||
+        pChoice === SCISSORS && cChoice === ROCK
+    ){return COMPUTER_WINS}
+    else {return RESULT_DRAW}
+}
 
 startGameBtn.addEventListener("click", function(){
     if (gameIsRunning){return;}
     gameIsRunning = true;
     console.log("Game is starting...");
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(playerChoice, computerChoice, winner);
 });
 
 //Restart section 131, prompt isn't working. Then push.
