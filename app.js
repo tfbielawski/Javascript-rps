98d40212c9e39810c0518dfd97b48fe61c1a448b
Tom Bielawski <tfbielawski@protonmail.com>
2024-01-26 11:26:01 -0500

    Converted functions to arrow functions

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const COMPUTER_WINS = "COMPUTER WINS";
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function(){
+const getPlayerChoice = () => {
     
     //JS prompt method, two args
     const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}?`, " ").toUpperCase();
@@ -26,21 +26,29 @@ const getPlayerChoice = function(){
     return selection;
 };
 
-const getComputerChoice = function(){
+const getComputerChoice = () => {
     const randomValue = Math.random();
     if (randomValue <= 0.34) {return ROCK;}
     else if (randomValue <= 0.67) {return PAPER;}
     else {return SCISSORS;}
 }
 
-const getWinner = function(cChoice, pChoice){
-    if (cChoice === pChoice){ return RESULT_DRAW; }
-    else if (
-        cChoice === ROCK && pChoice === PAPER || 
-        cChoice === PAPER && pChoice === SCISSORS ||
-        cChoice === SCISSORS && pChoice === ROCK
-    ){return PLAYER_WINS}
-    else {return COMPUTER_WINS}
+// const getWinner = function(cChoice, pChoice){
+//     if (cChoice === pChoice){ return RESULT_DRAW; }
+//     else if (
+//         cChoice === ROCK && pChoice === PAPER || 
+//         cChoice === PAPER && pChoice === SCISSORS ||
+//         cChoice === SCISSORS && pChoice === ROCK
+//     ){return PLAYER_WINS}
+//     else {return COMPUTER_WINS}
+// }
+
+const getWinner = (cChoice, pChoice) => {
+    cChoice === pChoice ? RESULT_DRAW : 
+    cChoice === ROCK && pChoice === PAPER || 
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK 
+    ? PLAYER_WINS : COMPUTER_WINS
 }
 
 startGameBtn.addEventListener("click", function(){
